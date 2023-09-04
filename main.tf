dea13df802649a9110ea72dcad6f4ac7ccb4a03c
nilanjana1994 <142746054+nilanjana1994@users.noreply.github.com>
2023-09-04 17:11:55 +0530

    commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+#Creating my s3 bucket, 
+resource "aws_s3_bucket" "bucket" {
+  bucket = "jenekinsnews3bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
